072e76c6d5c4040dec73375f93f03f3c324bd1fc
Renata Lopes <renatafslopes@gmail.com>
2023-01-06 23:26:20 -0300

    inclusão de dados

--- pro.py
+++ pro.py
@@ -7,3 +7,8 @@ co0 = "hFFFFFF" #white
 co1 = "#333333" #branco
 co2 = "#fcc058" #orande
 co3 = "#30576b" #valor
+
+co0 = "hFFFFFF" #white
+co1 = "#333333" #branco
+co2 = "#fcc058" #orande
+co3 = "#30576b" #valor
